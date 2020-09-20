601aee681871eed96e81a00a92aec1bc5851f081
sarakeskilohko <63452540+sarakeskilohko@users.noreply.github.com>
2020-09-20 13:58:29 +0300

    toimii/valmis + kommentoinnit

--- src/nimenKaantaja.java
+++ src/nimenKaantaja.java
@@ -1,6 +1,9 @@
 import java.util.*;
 public class nimenKaantaja {
     public static void main(String[] args) {
+
+        //* Luodaan scanneri, jolla luetaan annettu nimi, käännetään nimi for-loopin avulla
+
         Scanner lukija = new Scanner(System.in);
         System.out.println("Syötä nimi");
         String nimi = lukija.nextLine();
@@ -13,6 +16,8 @@ public class nimenKaantaja {
         System.out.println("Nimi toisinpäin on " + kaantynyt);
 
 
+        //*Poistetaan isot alkukirjaimet palindromitarkistusta varten
+
         nimi = nimi.toLowerCase();
         kaantynyt = kaantynyt.toLowerCase();
         /* if (nimi == kaantynyt) {
@@ -21,6 +26,8 @@ public class nimenKaantaja {
         else {
             System.out.println("Nimi ei ole palindromi");
         } <-- ei toimi */
+
+        //* Tarkistetaan, ovatko Stringit keskenään samat
 
         if (nimi.equals(kaantynyt)) {
             System.out.println("Nimi on palindromi");
